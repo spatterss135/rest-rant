4e7e3682df838cf4f18986932cb54b3bb23264ec
spatterss135 <spatterss135@gmail.com>
2022-01-05 16:52:27 -0600

    Added comment capability

--- views/specificPlace.jsx
+++ views/specificPlace.jsx
@@ -2,6 +2,11 @@ const React = require('react')
 const Def = require('./layouts/Default')
 
 function specificPlace ({place, index}) {
+    let rating = (
+        <h3 className="inactive">
+          0 stars
+        </h3>
+      )
     let comments = (
         <h3 className="inactive">
           No comments yet!
@@ -20,6 +25,13 @@ function specificPlace ({place, index}) {
             </div>
           )
         })
+
+        let sumRatings =  place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        rating = (
+            <div>{sumRatings/place.comments.length}</div>
+        )
       }
     return (
         <Def>
@@ -28,6 +40,7 @@ function specificPlace ({place, index}) {
                 <div className="card-body">
                     <h5 className="card-title">{place.name}</h5>
                     <p className="card-text">{place.city}, {place.state}</p>
+                    <p className="card-text">{rating}</p>
                     <p className="card-text">{comments}</p>
                     <a href={`/comments/${index}`}><button type="submit">Leave A Comment</button></a>
                     <form action={`/places/${index}?_method=DELETE`} method="post">
